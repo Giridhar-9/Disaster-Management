e0708bddae06b7767b76ba9e41a737b6ff923b05
unknown <you@example.com>
2025-04-12 23:05:43 +0530

    minor changes

--- measures.js
+++ measures.js
@@ -1,17 +1,17 @@
 
-  function openModal(id) {
-    document.getElementById(id).style.display = "block";
-  }
+function openModal(id) {
+  document.getElementById(id).style.display = "block";
+}
 
-  function closeModal(id) {
-    document.getElementById(id).style.display = "none";
-  }
+function closeModal(id) {
+  document.getElementById(id).style.display = "none";
+}
 
-  window.onclick = function(event) {
-    const modals = document.querySelectorAll('.modal');
-    modals.forEach(modal => {
-      if (event.target === modal) {
-        modal.style.display = "none";
-      }
-    });
-  }
+window.onclick = function (event) {
+  const modals = document.querySelectorAll('.modal');
+  modals.forEach(modal => {
+    if (event.target === modal) {
+      modal.style.display = "none";
+    }
+  });
+}
